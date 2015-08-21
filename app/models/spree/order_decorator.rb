5f587ffc96b1f2b40aa1d32731d6573da463e735
Viacheslav Mayorov <mayorovvn@gmail.com>
2015-08-21 13:25:09 +0300

    fix 'no payments' error when the order is payed fully by credits

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,6 +5,7 @@ Spree::Order.class_eval do
   # admin app when creating a new order from the admin console
   # In that case, we create an order before assigning a user
   before_save :process_store_credit, :if => "self.user.present? && @store_credit_amount"
+  after_save :persist_totals, :if => "self.user.present? && @store_credit_amount"
   after_save :ensure_sufficient_credit, :if => "self.user.present? && !self.completed?"
 
   validates_with StoreCreditMinimumValidator
